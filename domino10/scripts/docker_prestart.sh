10490fa888ccfaacdd14401408f483d57e69db6e
Thomas Hampel <thomas.hampel@de.ibm.com>
2018-11-22 11:44:03 +0100

    Adding Scripts for Domino build process

---
+++ domino10/scripts/docker_prestart.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#
+############################################################################
+# (C) Copyright IBM Corporation 2015, 2018                                 #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#      http://www.apache.org/licenses/LICENSE-2.0                          #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+
+if [ -z "$DominoMoveInstallData" ]; then
+  echo "No Template Install Directory specified"
+  exit 0
+fi
+
+if [ ! -r "$DominoMoveInstallData" ]; then
+  echo "Cannot read template install directory [$DominoMoveInstallData]"
+  exit 0
+fi
+
+echo "Copying templates at first run [Version $DominoVersion]"
+cp -R $DominoMoveInstallData/* /local/notesdata
+chown -R notes:notes /local/notesdata
+
+exit 0
