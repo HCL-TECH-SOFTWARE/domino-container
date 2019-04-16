565bbc9b76623c200c823c4c3dac85fe6917f811
Daniel-Nashed <nsh@nashcom.de>
2019-04-16 21:09:50 +0200

    Domino 10.0.1 FP1 and Domino CE Support
    - Added support for Domino Community Edition
    - Added support for Domino 10.0.1 FP1
    - Reorganized Build Scripts
    - Enhanced install and download error checking
    - Support for new software download check file "software.txt"
    - Script Logic now has no version dependency and also supports upcoming IF and HF versions.

--- dockerfiles/domino/build_domino.sh
+++ dockerfiles/domino/build_domino.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############################################################################
-# (C) Copyright IBM Corporation 2015, 2018                                 #
+# (C) Copyright IBM Corporation 2015, 2019                                 #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
@@ -19,32 +19,123 @@
 # Domino Docker Build Script
 # Usage  : ./build.sh <URL for download repository>
 # Example: ./build-image.sh http://192.168.1.1
+# ---------------------------------------------------
+# Optional Parameters in the following order
+# ---------------------------------------------------
+# Product Version
+# Product Fixpack
+# Product InterimsFix
+# (use "" for no Fixpack)
 
 SCRIPT_NAME=$0
 DOWNLOAD_FROM=$1
 
-DOCKER_IMAGE_NAME=ibmcom/domino
-DOCKER_IMAGE_VERSION=10.0.1
+# Select product to install
+#PROD_NAME=domino
+#PROD_NAME=domino-ce
+
+# Get product name from file name
+if [ -z $PROD_NAME ]; then
+  PROD_NAME=`basename $0 | cut -f 2 -d"_" | cut -f 1 -d"."`
+fi
+
+# Specify Version to install
+# Can be overwritten on command-line
+PROD_VER=10.0.1
+PROD_FP=FP1
+#PROD_HF=IF1
+#PROD_HF=HF123
+
+CUSTOM_VER=`echo "$2" | awk '{print toupper($0)}'`
+CUSTOM_FP=`echo "$3" | awk '{print toupper($0)}'`
+CUSTOM_HF=`echo "$4" | awk '{print toupper($0)}'`
+
+if [ ! -z "$CUSTOM_VER" ]; then
+  PROD_VER=$CUSTOM_VER
+  PROD_FP=$CUSTOM_FP
+  PROD_HF=$CUSTOM_HF
+fi
+
+DOCKER_IMAGE_NAME="ibmcom/$PROD_NAME"
+DOCKER_IMAGE_VERSION=$PROD_VER$PROD_FP$PROD_HF
 DOCKER_FILE=dockerfile_domino.txt
-#DominoBasePackage=domino10/DOMINO_SERVER_V10.0_64_BIT_LINUX_.tar
-DominoBasePackage=domino10/DOM_SVR_V10.0.1_64_BIT_Lnx.tar
+
+# Latest Tag not set when specifying explicit version
+
+if [ -z "$CUSTOM_VER" ]; then
+  DOCKER_TAG_LATEST="$DOCKER_IMAGE_NAME:latest"
+fi
 
 usage ()
 {
   echo
-  echo "Usage: `basename $SCRIPT_NAME` { domino }"
+  echo "Usage: `basename $SCRIPT_NAME` <URL for download repository> [DOMINO-VERSION] [FP] [IF/HF] "
   echo
   return 0
 }
+
+print_runtime()
+{
+  echo
+  
+  # the following line does not work on OSX 
+  # echo "Completed in" `date -d@$SECONDS -u +%T`
+ 
+  hours=$((SECONDS / 3600))
+  seconds=$((SECONDS % 3600))
+  minutes=$((seconds / 60))
+  seconds=$((seconds % 60))
+  h=""; m=""; s=""
+  if [ ! $hours =  "1" ] ; then h="s"; fi
+  if [ ! $minutes =  "1" ] ; then m="s"; fi
+  if [ ! $seconds =  "1" ] ; then s="s"; fi
+
+  if [ ! $hours =  0 ] ; then echo "Completed in $hours hour$h, $minutes minute$m and $seconds second$s"
+  elif [ ! $minutes = 0 ] ; then echo "Completed in $minutes minute$m and $seconds second$s"
+  else echo "Completed in $seconds second$s"; fi
+}
+
 
 docker_build ()
 {
   echo "Building Image : " $IMAGENAME
   
+  if [ -z "$DOCKER_TAG_LATEST" ]; then
+    DOCKER_IMAGE=$DOCKER_IMAGE_NAMEVERSION
+    DOCKER_TAG_LATEST_CMD=""
+  else
+    DOCKER_IMAGE=$DOCKER_TAG_LATEST
+    DOCKER_TAG_LATEST_CMD="-t $DOCKER_TAG_LATEST"
+  fi
+
+  # Get Build Time  
+  BUILDTIME=`date +"%d.%m.%Y %H:%M:%S"`
+
+
+	case "$PROD_NAME" in
+    domino)
+      DOCKER_DESCRIPTION="IBM Domino Enterprise Server"
+      ;;
+
+    domino_ce)
+      DOCKER_DESCRIPTION="IBM Domino Community Edition Server"
+      ;;
+
+    *)
+      error "Unknown product [$PROD_NAME] - Terminating installation"
+      exit 1
+      ;;
+  esac
+
+  
   # Get build arguments
   DOCKER_IMAGE=$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
-  BUILD_ARG_DOMINO_BASE_PACKAGE="--build-arg DominoBasePackage=$DominoBasePackage"
-  BUILD_ARG_DOWNLOAD_FROM="--build-arg DownloadFrom=$DOWNLOAD_FROM $BUILD_ARG"
+  
+  BUILD_ARG_PROD_NAME="--build-arg PROD_NAME=$PROD_NAME"
+  BUILD_ARG_PROD_VER="--build-arg PROD_VER=$PROD_VER"
+  BUILD_ARG_PROD_FP="--build-arg PROD_FP=$PROD_FP"
+  BUILD_ARG_PROD_HF="--build-arg PROD_HF=$PROD_HF"
+  BUILD_ARG_DOWNLOAD_FROM="--build-arg DownloadFrom=$DOWNLOAD_FROM"
 
   # Switch to current directory and remember current directory
   pushd .
@@ -52,7 +143,7 @@ docker_build ()
   cd $CURRENT_DIR
 
   # Finally build the image
-  docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION $DOCKER_TAG_LATEST_CMD -f $DOCKER_FILE $BUILD_ARG_DOWNLOAD_FROM $BUILD_ARG_DOMINO_BASE_PACKAGE .
+  docker build --no-cache --label "DominoDocker.description"="$DOCKER_DESCRIPTION" --label "DominoDocker.version"="$DOCKER_IMAGE_VERSION" --label "DominoDocker.buildtime"="$BUILDTIME" -t $DOCKER_IMAGE $DOCKER_TAG_LATEST_CMD -f $DOCKER_FILE $BUILD_ARG_DOWNLOAD_FROM $BUILD_ARG_PROD_NAME $BUILD_ARG_PROD_VER $BUILD_ARG_PROD_FP $BUILD_ARG_PROD_HF .
 
   popd
   echo
@@ -61,7 +152,7 @@ docker_build ()
   return 0
 }
 
-if [ "$DOWNLOAD_FROM" = "" ]; then
+if [ -z "$DOWNLOAD_FROM" ]; then
 	echo
 	echo "No download location specified!"
 	echo
@@ -70,5 +161,10 @@ if [ "$DOWNLOAD_FROM" = "" ]; then
 fi
 
 docker_build
+
+echo
+print_runtime
+echo
+
 exit 0
 
--- dockerfiles/domino/scripts/install_domino.sh
+++ dockerfiles/domino/scripts/install_domino.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ############################################################################
-# (C) Copyright IBM Corporation 2015, 2018                                 #
+# (C) Copyright IBM Corporation 2015, 2019                                 #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
@@ -29,63 +29,53 @@ export NUI_NOTESDIR=$LOTUS
 export PATH=$PATH:/local/notesdata
 export LANG=C
 
-CHECK_SOFTWARE_HASH_FILE=$INSTALL_DIR/software_dir_sha256.txt
+SOFTWARE_FILE=$INSTALL_DIR/software.txt
 WGET_COMMAND="wget --connect-timeout=20"
 
 # Helper Functions
 
-download_and_check_hash ()
+DOM_STRING_OK="Dominoserver Installation successful"
+LP_STRING_OK="Selected Language Packs are successfully installed."
+FP_STRING_OK="The installation completed successfully."
+HF_STRING_OK="The installation completed successfully."
+TRAVELER_STRING_OK="Installation completed with warnings."
+HF_UNINSTALL_STRING_OK="The installation completed successfully."
+JVM_STRING_OK="Patch was successfully applied."
+JVM_STRING_FP_OK="Tree diff file patch successful!"
+
+
+INST_DOM_LOG=/local/install_domino.log
+INST_FP_LOG=/local/install_fp.log
+INST_HF_LOG=/local/install_hf.log
+INST_TRAVELER_LOG=/local/install_traveler.log
+
+pushd()
 {
-  DOWNLOAD_FILE=$1
-
-  if [[ "$DOWNLOAD_FILE" =~ ".taz" ]]; then
-    TAR_OPTIONS=xz
-  elif [[ "$DOWNLOAD_FILE" =~ ".tar" ]]; then
-    TAR_OPTIONS=x
-  else
-    TAR_OPTIONS=""
-  fi
-
-  if [ -z "$TAR_OPTIONS" ]; then
-    # download without extracting for none tar files, without hash checking
-    echo
-      $WGET_COMMAND "$DOWNLOAD_FILE" 2>/dev/null
-    if [ "$?" = "0" ]; then
-      echo "Successfully downloaded: [$DOWNLOAD_FILE] "
-      return 0
-    else
-      echo "File [$DOWNLOAD_FILE] not downloaded correctly - terminating installation"
-      exit 1
-    fi
-  else
-    if [ -e $CHECK_SOFTWARE_HASH_FILE ]; then
-      HASH=`$WGET_COMMAND -qO- $DOWNLOAD_FILE | tee >(tar $TAR_OPTIONS) | sha256sum -b | cut -d" " -f1`
-      FOUND=`grep $HASH $CHECK_SOFTWARE_HASH_FILE | wc -l`
-
-      if [ "$FOUND" = "1" ]; then
-        echo "Successfully downloaded, extracted & checked: [$DOWNLOAD_FILE] "
-        return 0
-      else
-        echo "File [$DOWNLOAD_FILE] not downloaded correctly - terminating installation"
-        exit 1
-      fi
-    else
-      $WGET_COMMAND -qO- $DOWNLOAD_FILE | tar $TAR_OPTIONS 2>/dev/null
-
-      if [ "$?" = "0" ]; then
-        echo "Successfully downloaded & extracted: [$DOWNLOAD_FILE] "
-      else
-        echo "File [$DOWNLOAD_FILE] not downloaded correctly - terminating installation"
-        exit 1
-      fi
-    fi
-  fi
+  command pushd "$@" > /dev/null
 }
 
+popd ()
+{
+  command popd "$@" > /dev/null
+}
+
+export pushd popd
 
 print_delim ()
 {
   echo "--------------------------------------------------------------------------------"
+}
+
+log_ok ()
+{
+  echo "$1"
+}
+
+log_error ()
+{
+  echo
+  echo "Failed - $1"
+  echo
 }
 
 header ()
@@ -96,6 +86,188 @@ header ()
   print_delim
   echo
 }
+
+check_file_str ()
+{
+  CURRENT_FILE="$1"
+  CURRENT_STRING="$2"
+
+
+  if [ -e "$CURRENT_FILE" ]; then
+    CURRENT_RESULT=`grep "$CURRENT_STRING" "$CURRENT_FILE" ` 
+
+    if [ -z "$CURRENT_RESULT" ]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+
+  return 0
+}
+
+get_download_name ()
+{
+  DOWNLOAD_NAME=""
+  if [ -e "$SOFTWARE_FILE" ]; then
+    DOWNLOAD_NAME=`grep "$1|$2|" "$SOFTWARE_FILE" | cut -d"|" -f3`
+  else 
+    log_error "Download file [$SOFTWARE_FILE] not found!"
+    exit 1
+  fi
+
+  if [ -z "$DOWNLOAD_NAME" ]; then
+    log_error "Download for [$1] [$2] not found!"
+    exit 1
+  fi
+
+  return 0
+}
+
+download_file_ifpresent ()
+{
+  DOWNLOAD_SERVER=$1
+  DOWNLOAD_FILE=$2
+  TARGET_DIR=$3
+
+  if [ -z "$DOWNLOAD_FILE" ]; then
+    log_error "No download file specified!"
+    exit 1
+  fi
+
+  WGET_RET_OK=`$WGET_COMMAND -S --spider "$DOWNLOAD_SERVER/$DOWNLOAD_FILE" 2>&1 | grep 'HTTP/1.1 200 OK'`
+  if [ -z "$WGET_RET_OK" ]; then
+    echo "Download file does not exist [$DOWNLOAD_FILE]"
+    return 0
+  fi
+
+  pushd .
+  cd $TARGET_DIR
+
+  if [ -e "$DOWNLOAD_FILE" ]; then
+  	echo
+    echo "Replacing existing file [$DOWNLOAD_FILE]"
+    rm -f "$DOWNLOAD_FILE"
+  fi
+
+  $WGET_COMMAND "$DOWNLOAD_SERVER/$DOWNLOAD_FILE" 2>/dev/null
+
+  if [ "$?" = "0" ]; then
+    log_ok "Successfully downloaded: [$DOWNLOAD_FILE] "
+    echo
+    popd
+    return 0
+
+  else
+    log_error "File [$DOWNLOAD_FILE] not downloaded correctly"
+    popd
+    exit 1
+  fi
+}
+
+
+download_and_check_hash ()
+{
+  DOWNLOAD_FILE=$1
+  TARGET_DIR=$2
+
+  if [ -z "$DOWNLOAD_FILE" ]; then
+    log_error "No download file specified!"
+    exit 1
+  fi
+
+  # check if file exists before downloading
+
+  WGET_RET_OK=`$WGET_COMMAND -S --spider "$DOWNLOAD_FILE" 2>&1 | grep 'HTTP/1.1 200 OK'`
+  if [ -z "$WGET_RET_OK" ]; then
+    log_error "File [$DOWNLOAD_FILE] does not exist"
+    exit 1
+  fi
+
+  pushd .
+
+  if [ ! -z "$TARGET_DIR" ]; then
+    mkdir -p "$TARGET_DIR"
+    cd "$TARGET_DIR"
+  fi
+
+  if [[ "$DOWNLOAD_FILE" =~ ".tar.gz" ]]; then
+    TAR_OPTIONS=xz
+  elif [[ "$DOWNLOAD_FILE" =~ ".taz" ]]; then
+    TAR_OPTIONS=xz
+  elif [[ "$DOWNLOAD_FILE" =~ ".tar" ]]; then
+    TAR_OPTIONS=x
+  else
+    TAR_OPTIONS=""
+  fi
+
+  if [ -z "$TAR_OPTIONS" ]; then
+
+    # download without extracting for none tar files, without hash checking
+    $WGET_COMMAND "$DOWNLOAD_FILE" 2>/dev/null
+
+    if [ "$?" = "0" ]; then
+      log_ok "Successfully downloaded: [$DOWNLOAD_FILE] "
+      popd
+      return 0
+
+    else
+      log_error "File [$DOWNLOAD_FILE] not downloaded correctly [1]"
+      popd
+      exit 1
+    fi
+  else
+    if [ -e $SOFTWARE_FILE ]; then
+      HASH=`$WGET_COMMAND -qO- $DOWNLOAD_FILE | tee >(tar $TAR_OPTIONS 2>/dev/null) | sha256sum -b | cut -d" " -f1`
+      FOUND=`grep $HASH $SOFTWARE_FILE | wc -l`
+
+      if [ "$FOUND" = "1" ]; then
+        log_ok "Successfully downloaded, extracted & checked: [$DOWNLOAD_FILE] "
+        popd
+        return 0
+
+      else
+        log_error "File [$DOWNLOAD_FILE] not downloaded correctly [2]"
+        popd
+        exit 1
+      fi
+    else
+      $WGET_COMMAND -qO- $DOWNLOAD_FILE | tar $TAR_OPTIONS 2>/dev/null
+
+      if [ "$?" = "0" ]; then
+        log_ok "Successfully downloaded & extracted: [$DOWNLOAD_FILE] "
+        popd
+        return 0
+
+      else
+        log_error "File [$DOWNLOAD_FILE] not downloaded correctly [3]"
+        popd
+        exit 1
+      fi
+    fi
+  fi
+
+  popd
+  return 0
+}
+
+check_binary_busy()
+{
+  if [ ! -e "$1" ]; then
+    return 0
+  fi
+
+  TARGET_REAL_BIN=`readlink -f $1`
+  FOUND_TARGETS=`lsof | awk '{print $9}' | grep "$TARGET_REAL_BIN"`
+
+  if [ -n "$FOUND_TARGETS" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+
 
 check_file_busy()
 {
@@ -231,10 +403,212 @@ install_res_links ()
   return 0
 }
 
+
+install_domino ()
+{
+  INST_VER=$PROD_VER
+
+  if [ -z "$PROD_FP" ]; then
+    INST_FP=""
+  else
+    INST_FP=$PROD_VER$PROD_FP
+  fi
+
+  if [ -z "$PROD_HF" ]; then
+    INST_HF=""
+  else
+    INST_HF=$PROD_VER$PROD_FP$PROD_HF
+  fi
+
+  echo
+  echo "Downloading Domino Installation files ..."
+  echo
+  
+  # download start script
+  download_and_check_hash $DownloadFrom/start_script.tar
+  
+  if [ ! -z "$INST_VER" ]; then
+    get_download_name $PROD_NAME $INST_VER
+    download_and_check_hash $DownloadFrom/$DOWNLOAD_NAME domino_server
+  fi
+
+  if [ ! -z "$INST_FP" ]; then
+    get_download_name $PROD_NAME $INST_FP domino_fp
+    download_and_check_hash $DownloadFrom/$DOWNLOAD_NAME domino_fp
+  fi
+
+  if [ ! -z "$INST_HF" ]; then
+    get_download_name $PROD_NAME $INST_HF domino_hf
+    download_and_check_hash $DownloadFrom/$DOWNLOAD_NAME domino_hf
+  fi
+
+  if [ ! -z "$INST_VER" ]; then
+    header "Installing $PROD_NAME $INST_VER"
+    pushd .
+
+    case "$PROD_NAME" in
+      domino)
+        cd domino_server/linux64/domino
+        ;;
+
+      domino-ce)
+        cd domino_server/linux64/DominoEval
+      ;;
+
+      *)
+        log_error "Unknown product [$PROD_NAME] - Terminating installation"
+        popd
+        exit 1
+        ;;
+    esac
+
+    echo
+    echo "Running Domino Silent Install -- This takes a while ..."
+    echo
+
+    ./install -silent -options "$INSTALL_DIR/$DominoResponseFile"
+
+    echo $INST_VER >/local/notesdata/data_version.txt
+
+    mv "$Notes_ExecDirectory/DominoInstall.log" "$INST_DOM_LOG"
+
+    check_file_str "$INST_DOM_LOG" "$DOM_STRING_OK"
+
+    if [ "$?" = "1" ]; then
+      echo
+      log_ok "Domino installed successfully"
+
+    else
+      print_delim
+      cat $INST_DOM_LOG
+      print_delim
+
+      log_error "Domino Installation failed!!!"
+      popd
+      exit 1
+    fi
+
+    popd
+    rm -rf domino_server
+  fi
+
+  if [ ! -z "$INST_FP" ]; then
+    header "Installing Fixpack $INST_FP"
+
+    echo
+    echo "Running Domino Fixpack Silent Install -- This takes a while ..."
+    echo
+
+    pushd .
+    cd domino_fp/linux64/domino
+
+    ./install -script script.dat > $INST_FP_LOG
+
+    echo $INST_FP >/local/notesdata/data_version.txt
+
+    check_file_str "$INST_FP_LOG" "$FP_STRING_OK"
+
+    if [ "$?" = "1" ]; then
+    	echo
+      log_ok "Fixpack installed successfully"
+    else
+
+      echo
+      print_delim
+      cat $INST_FP_LOG
+      print_delim
+      log_error "Fixpack Installation failed!!!"
+      popd
+      exit 1
+    fi
+
+    popd
+    rm -rf domino_fp
+
+  fi  
+
+  if [ ! -z "$INST_HF" ]; then
+    header "Installing IF/HF INST_HF"
+
+    echo
+    echo "Running Domino Iterimsfix/Hotfix Silent Install -- This takes a while ..."
+    echo
+
+    pushd .
+    cd domino_hf/linux64
+
+    ./install -script script.dat > $INST_HF_LOG
+
+    check_file_str "$INST_HF_LOG" "$HF_STRING_OK"
+
+    if [ "$?" = "1" ]; then
+      echo
+      log_ok "InterimsFix/HotFix installed successfully"
+    else
+
+      echo
+      print_delim
+      cat hf.log
+      print_delim
+      log_error "InterimsFix/HotFix Installation failed!!!"
+
+      popd
+      exit 1
+    fi
+
+    popd
+    rm -rf domino_hf
+
+  fi
+
+  return 0
+}
+
+install_traveler ()
+{
+  header "$PROD_NAME Installation"
+
+  if [ ! -z "$INST_VER" ]; then
+    get_download_name $PROD_NAME $INST_VER
+    download_and_check_hash $DownloadFrom/$DOWNLOAD_NAME traveler
+  fi
+
+  header "Installing $PROD_NAME $INST_VER"
+
+  pushd .
+
+  cd traveler
+
+  ./silentInstall > $INST_TRAVELER_LOG
+
+  cp /local/notesdata/IBM_TECHNICAL_SUPPORT/traveler/logs/TravelerInstall.log /local
+
+  check_file_str "$INST_TRAVELER_LOG" "$TRAVELER_STRING_OK"
+
+  if [ "$?" = "1" ]; then
+    echo
+    log_ok "Traveler installed successfully"
+
+  else
+
+    print_delim
+    cat $INST_TRAVELER_LOG
+    print_delim
+
+    log_error "Traveler Installation failed!!!"
+    exit 1
+  fi
+
+  popd
+  rm -rf traveler 
+
+  return 0
+}
+
+
 # --- Main Install Logic ---
 
 # Add notes:notes user
-
 if [ -z "$DominoUserID" ]; then
   adduser notes -U
 else
@@ -257,7 +631,10 @@ header "Environment Setup"
 
 echo "INSTALL_DIR           = [$INSTALL_DIR]"
 echo "DownloadFrom          = [$DownloadFrom]"
-echo "DominoBasePackage     = [$DominoBasePackage]"
+echo "Product               = [$PROD_NAME]"
+echo "Version               = [$PROD_VER]"
+echo "Fixpack               = [$PROD_FP]"
+echo "InterimsFix/Hotfix    = [$PROD_HF]"
 echo "DominoResponseFile    = [$DominoResponseFile]"
 echo "DominoMoveInstallData = [$DominoMoveInstallData]"
 echo "DominoVersion         = [$DominoVersion]"
@@ -268,27 +645,35 @@ create_directory /local/notesdata notes notes 770
 create_directory /local/translog notes notes 770
 create_directory /local/daos notes notes 770
 
-header "IBM Domino Base Install"
-
-# Download Domino Server install files
 cd "$INSTALL_DIR"
 
-echo
-echo "Downloading Domino Installation files ..."
-echo
+header "Environment"
 
-download_and_check_hash $DownloadFrom/$DominoBasePackage
-download_and_check_hash $DownloadFrom/start_script.tar
+df -h
 
 echo
-echo "Running Domino Silent Install -- This takes a while ..."
+print_delim
 echo
+
+# Download updated software.txt file if available
+download_file_ifpresent "$DownloadFrom" software.txt "$INSTALL_DIR"
+
+case "$PROD_NAME" in
+  domino|domino-ce)
+    install_domino
+    ;;
+
+  traveler)
+    install_traveler
+    ;;
+
+  *)
+    log_error "Unknown product [$PROD_NAME] - Terminating installation"
+    exit 1
+    ;;
+esac
 
 header "Final Steps & Configuration"
-
-# Run Domino Silent Install
-cd "$INSTALL_DIR/linux64/domino/"
-./install -silent -options "$INSTALL_DIR/$DominoResponseFile"
 
 # Removing Base Install files
 cd "$INSTALL_DIR"
@@ -320,6 +705,10 @@ if [ ! -z "$DominoMoveInstallData" ]; then
   create_directory /local/notesdata notes notes 770
 fi
 
+# Remove Fixpack/Hotfix Backup Files
+
+find $Notes_ExecDirectory -maxdepth 1 -type d -name "100**" -exec rm -rf {} \;
+
 # Create missing links
 cd /opt/ibm/domino/bin/
 ln -f -s tools/startup kyrtool
@@ -329,8 +718,8 @@ install_res_links
 # Remove tunekernel. It is causing error messages because of Docker virtualization
 rm -f /opt/ibm/domino/notes/latest/linux/tunekrnl
 
-remove_directory "$INSTALL_DIR"
+# remove_directory "$INSTALL_DIR"
 
-header "Done Install Domino"
+header "Successfully completed installation!"
 
 exit 0
