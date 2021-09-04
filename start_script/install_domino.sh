c12d634c0638fba01a593545fd8039d60639307a
Daniel-Nashed <nsh@nashcom.de>
2021-09-04 06:33:22 +0200

    install check for existing version and update options

--- start_script/install_domino.sh
+++ start_script/install_domino.sh
@@ -26,6 +26,7 @@ VERSION_FILE_NAME_URL=$DOMINO_DOCKER_GIT_URL/software/current_version.txt
 SOFTWARE_FILE=$SOFTWARE_DIR/software.txt
 VERSION_FILE=$SOFTWARE_DIR/current_version.txt
 LOTUS=/opt/hcl/domino
+PROD_VER_FILE=$LOTUS/DominoVersionInstalled.txt
 
 SPECIAL_CURL_ARGS=
 CURL_CMD="curl --fail --location --connect-timeout 15 --max-time 300 $SPECIAL_CURL_ARGS"
@@ -37,7 +38,9 @@ print_delim ()
 
 log_ok ()
 {
+  echo
   echo "$1"
+  echo
 }
 
 log_error ()
@@ -130,7 +133,7 @@ download_file_ifpresent ()
   STATUS_RET=$(echo $CURL_RET | grep -e 'HTTP/1.1 200 OK' -e 'HTTP/2 200')
   if [ -z "$STATUS_RET" ]; then
 
-    echo "Info: Download file does not exist [$DOWNLOAD_FILE]"
+    log_ok "Info: Download file does not exist [$DOWNLOAD_FILE]"
     return 0
   fi
 
@@ -141,8 +144,7 @@ download_file_ifpresent ()
   fi
 
   if [ -e "$DOWNLOAD_FILE" ]; then
-  	echo
-    echo "Replacing existing file [$DOWNLOAD_FILE]"
+    log_ok "Replacing existing file [$DOWNLOAD_FILE]"
     rm -f "$DOWNLOAD_FILE"
   fi
 
@@ -152,7 +154,6 @@ download_file_ifpresent ()
 
   if [ "$?" = "0" ]; then
     log_ok "Successfully downloaded: [$DOWNLOAD_FILE] "
-    echo
     popd
     return 0
 
@@ -290,13 +291,13 @@ get_current_version ()
   fi
 
   if [ -n "$DOWNLOAD_VERSION_FILE" ]; then
-    echo "Getting current software version from [$DOWNLOAD_VERSION_FILE]"
+    log_ok "Getting current software version from [$DOWNLOAD_VERSION_FILE]"
     LINE=`$CURL_CMD -L --silent $DOWNLOAD_VERSION_FILE | grep "^$1|"`
   else
     if [ ! -r "$VERSION_FILE" ]; then
-      echo "No current version file found! [$VERSION_FILE]"
+      log_ok "No current version file found! [$VERSION_FILE]"
     else
-      echo "Getting current software version from [$VERSION_FILE]"
+      log_ok "Getting current software version from [$VERSION_FILE]"
       LINE=`grep "^$1|" $VERSION_FILE`
     fi
   fi
@@ -438,7 +439,7 @@ install_start_script()
   # Downloads and installs the latest Domino start script from the Domino Docker Community image GitHub repo
 
   cd $SOFTWARE_DIR 
-  $CURL_CMD -L $START_SCRIPT_URL -o start_script.tar
+  $CURL_CMD -sL $START_SCRIPT_URL -o start_script.tar
 
   if [ -e start_script ]; then
     rm -rf start_script
@@ -453,14 +454,35 @@ install_domino()
 {
   header "Install Domino"
 
-  if [ -e "$LOTUS/bin/server" ]; then
-    echo "Domino already installed"
-    return 0
-  fi
-
-  # Determines current version
+  # If no version was speficed find current version
   if [ -z "$PROD_VER" ]; then
     get_current_version $PROD_NAME 
+  fi
+
+  if [ -e "$LOTUS/bin/server" ]; then
+
+    # If Domino was installed by this routine, there is a version file
+    if [ -e "$PROD_VER_FILE" ]; then
+  
+      PROD_VER_INSTALLED=$(head -1 $PROD_VER_FILE)
+
+      if [ "$PROD_FORCE_INSTALL" = "yes" ]; then
+        log_ok "Re-installing Domino $PROD_VER"
+
+      elif [ "$PROD_VER" = "$PROD_VER_INSTALLED" ]; then
+        log_ok "Domino $PROD_VER already installed"
+        return 0
+
+      else
+        log_ok "Updating Domino $PROD_VER_INSTALLED -> $PROD_VER"
+      fi
+    fi
+
+    log_ok "Domino already installed"
+    return 0
+
+  else
+    log_ok "Installing Domino $PROD_VER"
   fi
 
   # Gets download name stored in GitHub repo 
@@ -505,6 +527,8 @@ install_domino()
   
   cd $SOFTWARE_DIR 
   rm -rf linux64
+
+  echo $PROD_VER > $PROD_VER_FILE
 
 }
 
